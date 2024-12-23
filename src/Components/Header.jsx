008e6d80f3a30c33413f3386a4d35cd30f4b2f7c
mahdi056 <ahnafmahdi12@gmail.com>
2024-12-23 12:11:05 +0600

    responsive navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import Navbar from "./Navbar";
 import { FaUserAlt } from "react-icons/fa";
+import { FiAlignJustify } from "react-icons/fi";
 
 const Header = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false); 
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);  
+    };
+
     return (
         <div className="mt-4 w-4/5 flex justify-between mx-auto">
 
-            <div className="text-2xl font-bold text-blue-500">
+            <div className="text-2xl font-bold text-blue-500 hidden md:block">
                 Artifact Atlas
             </div>
 
-            <div>
+            <div className="md:hidden" >
+                <FiAlignJustify onClick={toggleMenu} className="cursor-pointer text-sm"></FiAlignJustify>
+            </div>
 
+
+
+            <div className={`${menuOpen ? 'flex flex-col' : 'hidden'} md:block`}>
                 <Navbar></Navbar>
             </div>
 
