c80656e82d5cb50b83ea490071448241c3a65bd2
mahdi056 <ahnafmahdi12@gmail.com>
2024-12-23 10:48:30 +0600

    banner setup

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,37 @@
-
+import banner1 from "../assets/banner1.webp";
+import banner2 from "../assets/banner2.webp";
+import banner3 from "../assets/banner3.jpg";
 
 const Home = () => {
     return (
         <div className="mt-8">
+           <div className="mx-auto flex justify-center">
 
-            
-            
+           <div className="carousel w-4/5">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={banner1} className="w-full object-cover" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={banner2} className="w-full object-cover" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={banner3} className="w-full object-cover" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+
+           </div>
         </div>
     );
 };
